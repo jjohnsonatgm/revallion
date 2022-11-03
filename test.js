606ff8efe913e9dfd7bd9a9e3e6a35b6886b4b91
Joshua Johnson <Joshua.Johnson@greatminds.org>
2022-11-03 12:26:20 -0400

    test browserstack

--- test.js
+++ test.js
@@ -1,6 +1,16 @@
-
 require('dotenv').config();
+import { t } from 'testcafe';
 
 console.log('Test');
 console.log(process.env.BROWSERSTACK_BUILD_ID);
 console.log(process.env.BROWSERSTACK_PROJECT_NAME);
+
+fixture('Getting Started')
+  .page('https://devexpress.github.io/testcafe/example');
+
+test('My first test', async t => {
+  await t
+    .typeText('#developer-name', process.env.BROWSERSTACK_USERNAME)
+    .typeText('#developer-name', process.env.BROWSERSTACK_ACCESS_KEY)
+    .click('#submit-button');
+});
